c370c5c9acde780ed0bfc17aab95b83f910c3402
AnnaBinto <129248718+AnnaBinto@users.noreply.github.com>
2023-11-06 15:17:10 +0530

    Add files via upload

---
+++ calculator.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main()
+{
+    int sum,a,b;
+    printf("enter the first number");
+    scanf("%d",&a);
+    printf("enter the second number");
+    scanf("%d,&b")
+    sum=a+b;
+    printf("the sum is :",sum)
+}
